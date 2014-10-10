6df7e231dd0bd86e69521fa9121b4916b60d59e0
Fortyseven <fortyseven@gmail.com>
2014-10-10 07:56:07 -0400

    Initial commit

---
+++ Assets/MoleculeA.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoleculeA : MonoBehaviour
+{
+	private float MAXSPEED = 500.0f;
+
+	void Start ()
+	{
+		rigidbody2D.AddForce (new Vector2 (Random.Range (-MAXSPEED, MAXSPEED), Random.Range (-MAXSPEED, MAXSPEED)));
+	}
+}
---
+++ Assets/SimInit.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimInit : MonoBehaviour
+{
+	private const int MAX_MOLECULES = 1500;
+
+	public GameObject MoleculeAPrefab;
+	public  GameObject Walls;
+
+	private ArrayList _moleculesA;
+
+	void Start ()
+	{
+		_moleculesA = new ArrayList ();
+
+		for (var i = 0; i < MAX_MOLECULES; i++) {
+			Vector3 pos = new Vector3 (
+						Random.Range (-9, 9) + (Random.Range (0, 1000) / 1000), 
+						Random.Range (-9, 9) + (Random.Range (0, 1000) / 1000), 
+						0);
+			_moleculesA.Add (Instantiate (MoleculeAPrefab, pos, Quaternion.identity));
+		}
+	}
+
+	void Update()
+	{
+		if (Input.anyKeyDown) {
+			Walls.SetActive(!Walls.activeInHierarchy);
+		}
+	}
+}
